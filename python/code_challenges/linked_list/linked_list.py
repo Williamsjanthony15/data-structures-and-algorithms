a3ce6045e9c4043a1f605c2d0a36aadbf39f0329
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-06-22 00:42:33 -0500

    passing 9 tests

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -1,78 +1,76 @@
 class Node:
-    def __init__(self, value, next = None):
+    def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
 
-    def __str__(self):
-        return str(self.value)
-
 class LinkedList:
-    def __init__(self, head = None):
-        self.head = head
+
+    def __init__(self, node=None):
+        self.head = node
 
     def insert(self, value):
         node = Node(value)
+        node.next = self.head
+        self.head = node
+        return self
 
-        if self.head is not None:
-            node.next = self.head # -> pears, -> oranges, -> apples
-        self.head = node #HEAD: pears -> oranges -> apples
-
-    def __str__ (self):
-        list_input = []
-        if self.head is None:
-            return  "there is no node value"
+    def includes(self, target):
         current = self.head
-
-        while current is not None:
-            list_input.append(current.value)
-            current = current.next
-
-
-    def includes(self, value):
-        current = self.head
-
-        while current.value is not None:
-            if current.value == value:
+        while current:
+            if current.value == target:
                 return True
             current = current.next
         return False
 
+    def __str__(self):
+        string = ""
+        current = self.head
+        while current:
+            string += f"{ {current.value} } -> "
+            current = current.next
+        string += " None "
+        return string
 
-#
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return self
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
+        return self
 
+    def insert_before(self, target, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            return None
+        if self.head.value == target:
+            new_node.next = self.head
+            self.head = new_node
+            return self
+        current = self.head
+        while current is not None:
+            if current.next.value == target:
+                new_node.next = current.next
+                current.next = new_node
+                return self
+            current = current.next
+        print("Target not within list")
 
+    def insert_after(self, target, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            return None
+        current = self.head
+        while current is not None:
+            if current.value == target:
+                new_node.next = current.next
+                current.next = new_node
+                return self
+            current = current.next
+        print("Target not within list")
 
-
-# current = ll2.head
-# while current.value is not None:
-#     do something
-#     current = current.next
-
-# if __name__ == "__main__":
-#     ll2 = LinkedList()
-#     ll2.insert('apples')
-#     ll2.insert('orange')
-#     ll2.insert('pears')
-
-# function link list head -> apples? / nope -> apples yet? / nope -> apples yet? / true
-# if linked list's value becomes none, That is the end of the linked list.
-
-## while loop  - if value is in the linked list, return items, if not than return None
-# THIS IS HOW TO DO IT! MAIN WAY TO DO THIS!
-
-
-
-# Manual long route
-#     ll = LinkedList()
-#     node1 = Node('apples')
-#     node2 = Node('orange')
-#     node3 = Node('pear')
-#     ll.head == node1
-#     node1.next == node2
-#     node2.next == node3
-
-# Should return this llhead = apples -> oranges -> pear -> None
-
-# ll1 = LinkedList(Node('apples', Node('Oranges', Node('Pear'))))
-# Single line on how to do it. BUT you cant reference inside the initial Node
-
+if __name__ == "__main__":
+    pass

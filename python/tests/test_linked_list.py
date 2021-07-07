0b9475dbb7d2cc6c10cec29b0995cbde050d0562
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-07-07 00:14:40 -0500

    finished pushing testing, Working on popping multiple. Cant figure out how to create multiple without doing it 1 by 1

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,146 +1,146 @@
-from code_challenges.linked_list.linked_list import LinkedList, Node
-# from LinkedList import includes
+# from code_challenges.linked_list.linked_list import LinkedList, Node
+# # from LinkedList import includes
 
 
-def test_node_instance():
-    node = Node('apples', None)
-    actual = node.value
-    expected = 'apples'
-    assert actual == expected
-    assert node.next == None
+# def test_node_instance():
+#     node = Node('apples', None)
+#     actual = node.value
+#     expected = 'apples'
+#     assert actual == expected
+#     assert node.next == None
 
-def test_two_node():
-    node2 = Node('orange', None)
-    node1 = Node('apples', node2)
-    actual = node1.next.value
-    expected = 'orange'
-    assert actual == expected
+# def test_two_node():
+#     node2 = Node('orange', None)
+#     node1 = Node('apples', node2)
+#     actual = node1.next.value
+#     expected = 'orange'
+#     assert actual == expected
 
-def test_empty_ll():
-    ll = LinkedList()
-    assert ll
+# def test_empty_ll():
+#     ll = LinkedList()
+#     assert ll
 
-def insert_one():
-    ll = LinkedList()
-    ll.insert('apples')
-    actual = ll.head.value
-    expected = 'apples'
-    assert actual == expected
+# def insert_one():
+#     ll = LinkedList()
+#     ll.insert('apples')
+#     actual = ll.head.value
+#     expected = 'apples'
+#     assert actual == expected
 
-def pointer():
-    ll = LinkedList()
-    ll.insert('a')
-    ll.insert('b')
-    ll.insert('c')
-    actual = ll.head.value
-    expected = 'c'
-    assert actual == expected
+# def pointer():
+#     ll = LinkedList()
+#     ll.insert('a')
+#     ll.insert('b')
+#     ll.insert('c')
+#     actual = ll.head.value
+#     expected = 'c'
+#     assert actual == expected
 
-def true_search():
-    ll = LinkedList(Node('apples', Node('oranges', Node('bananas', Node('cherry')))))
-    actual = ll.includes('cherry')
-    expected = True
-    assert actual == expected
+# def true_search():
+#     ll = LinkedList(Node('apples', Node('oranges', Node('bananas', Node('cherry')))))
+#     actual = ll.includes('cherry')
+#     expected = True
+#     assert actual == expected
 
 
-def false_search():
-    ll = LinkedList(Node('apples', Node('oranges', Node('bananas', Node('cherry')))))
-    actual = ll.includes('watermelon')
-    expected = False
-    assert actual == expected
+# def false_search():
+#     ll = LinkedList(Node('apples', Node('oranges', Node('bananas', Node('cherry')))))
+#     actual = ll.includes('watermelon')
+#     expected = False
+#     assert actual == expected
 
-def test_can_succesfully_add_a_node_to_the_end_of_the_list():
-    ll1 = LinkedList()
-    ll1.insert('a').insert('b').insert('c').insert('d')
-    ll1.append('Z')
-    expected = ['d', 'c', 'b', 'a', 'Z']
-    current = ll1.head
-    index = 0
-    while current is not None:
-        assert current.value == expected[index]
-        index+=1
-        current = current.next
+# def test_can_succesfully_add_a_node_to_the_end_of_the_list():
+#     ll1 = LinkedList()
+#     ll1.insert('a').insert('b').insert('c').insert('d')
+#     ll1.append('Z')
+#     expected = ['d', 'c', 'b', 'a', 'Z']
+#     current = ll1.head
+#     index = 0
+#     while current is not None:
+#         assert current.value == expected[index]
+#         index+=1
+#         current = current.next
 
-def test_can_sucessfully_add_multiple_nodes_to_the_end_of_list():
-    ll1 = LinkedList()
-    ll1.append('a').append('b').append('c').append('d')
-    expected = ['a', 'b', 'c', 'd']
-    current = ll1.head
-    index = 0
-    while current is not None:
-        assert current.value == expected[index]
-        index+=1
-        current = current.next
+# def test_can_sucessfully_add_multiple_nodes_to_the_end_of_list():
+#     ll1 = LinkedList()
+#     ll1.append('a').append('b').append('c').append('d')
+#     expected = ['a', 'b', 'c', 'd']
+#     current = ll1.head
+#     index = 0
+#     while current is not None:
+#         assert current.value == expected[index]
+#         index+=1
+#         current = current.next
 
-def test_can_succesfully_insert_a_node_before_a_node_in_middle_of_list():
-    ll1 = LinkedList()
-    ll1.append('a').append('b').append('c').append('d')
-    ll1.insert_before('c', 'Z')
-    expected = ['a', 'b', 'Z', 'c', 'd']
-    current = ll1.head
-    index = 0
-    while current is not None:
-        assert current.value == expected[index]
-        index+=1
-        current = current.next
+# def test_can_succesfully_insert_a_node_before_a_node_in_middle_of_list():
+#     ll1 = LinkedList()
+#     ll1.append('a').append('b').append('c').append('d')
+#     ll1.insert_before('c', 'Z')
+#     expected = ['a', 'b', 'Z', 'c', 'd']
+#     current = ll1.head
+#     index = 0
+#     while current is not None:
+#         assert current.value == expected[index]
+#         index+=1
+#         current = current.next
 
-def test_can_succesfully_insert_a_node_before_the_first_node_of_a_linked_list():
-    ll1 = LinkedList()
-    ll1.append('a').append('b').append('c').append('d')
-    ll1.insert_before('a', 'Z')
-    expected = ['Z', 'a', 'b', 'c', 'd']
-    current = ll1.head
-    index = 0
-    while current is not None:
-        assert current.value == expected[index]
-        index+=1
-        current = current.next
+# # def test_can_succesfully_insert_a_node_before_the_first_node_of_a_linked_list():
+# #     ll1 = LinkedList()
+# #     ll1.append('a').append('b').append('c').append('d')
+# #     ll1.insert_before('a', 'Z')
+# #     expected = ['Z', 'a', 'b', 'c', 'd']
+# #     current = ll1.head
+# #     index = 0
+# #     while current is not None:
+# #         assert current.value == expected[index]
+# #         index+=1
+# #         current = current.next
 
-def test_Can_successfully_insert_after_a_node_in_the_middle_of_the_linked_list():
-    ll1 = LinkedList()
-    ll1.append('a').append('b').append('c').append('d')
-    ll1.insert_after('b', 'Z')
-    expected = ['a', 'b', 'Z', 'c', 'd']
-    current = ll1.head
-    index = 0
-    while current is not None:
-        assert current.value == expected[index]
-        index+=1
-        current = current.next
+# def test_Can_successfully_insert_after_a_node_in_the_middle_of_the_linked_list():
+#     ll1 = LinkedList()
+#     ll1.append('a').append('b').append('c').append('d')
+#     ll1.insert_after('b', 'Z')
+#     expected = ['a', 'b', 'Z', 'c', 'd']
+#     current = ll1.head
+#     index = 0
+#     while current is not None:
+#         assert current.value == expected[index]
+#         index+=1
+#         current = current.next
 
-def test_Can_successfully_insert_a_node_after_the_last_node_of_the_linked_list():
-    ll1 = LinkedList()
-    ll1.append('a').append('b').append('c').append('d')
-    ll1.insert_after('d', 'Z')
-    expected = ['a', 'b', 'c', 'd', 'Z']
-    current = ll1.head
-    index = 0
-    while current is not None:
-        assert current.value == expected[index]
-        index+=1
-        current = current.next
+# def test_Can_successfully_insert_a_node_after_the_last_node_of_the_linked_list():
+#     ll1 = LinkedList()
+#     ll1.append('a').append('b').append('c').append('d')
+#     ll1.insert_after('d', 'Z')
+#     expected = ['a', 'b', 'c', 'd', 'Z']
+#     current = ll1.head
+#     index = 0
+#     while current is not None:
+#         assert current.value == expected[index]
+#         index+=1
+#         current = current.next
 
-def test_kth_from_end_works():
-    my_list = LinkedList(Node('z' ,(Node('a', Node( 'b', Node( 'c'))))))
-    actual = my_list.kth_from_end(2)
-    expected = 'a'
-    assert actual == expected
+# def test_kth_from_end_works():
+#     my_list = LinkedList(Node('z' ,(Node('a', Node( 'b', Node( 'c'))))))
+#     actual = my_list.kth_from_end(2)
+#     expected = 'a'
+#     assert actual == expected
 
-def test_kth_from_end_k_too_large():
-    my_list = LinkedList(Node('z' ,(Node('a', Node( 'b', Node( 'c'))))))
-    actual = my_list.kth_from_end(4)
-    expected = 'choose a smaller number'
-    assert actual == expected
+# def test_kth_from_end_k_too_large():
+#     my_list = LinkedList(Node('z' ,(Node('a', Node( 'b', Node( 'c'))))))
+#     actual = my_list.kth_from_end(4)
+#     expected = 'choose a smaller number'
+#     assert actual == expected
 
-def test_kth_from_end_k_negative():
-    my_list = LinkedList(Node('z' ,(Node('a', Node( 'b', Node( 'c'))))))
-    actual = my_list.kth_from_end(-2)
-    expected = 'Choose a positive number'
-    assert actual == expected
+# def test_kth_from_end_k_negative():
+#     my_list = LinkedList(Node('z' ,(Node('a', Node( 'b', Node( 'c'))))))
+#     actual = my_list.kth_from_end(-2)
+#     expected = 'Choose a positive number'
+#     assert actual == expected
 
-def test_kth_from_end_list_only_1():
-    my_list = LinkedList()
-    my_list.insert('matthew')
-    actual = my_list.kth_from_end(2)
-    expected = 'List only has 1 item'
-    assert actual == expected
+# def test_kth_from_end_list_only_1():
+#     my_list = LinkedList()
+#     my_list.insert('matthew')
+#     actual = my_list.kth_from_end(2)
+#     expected = 'List only has 1 item'
+#     assert actual == expected
--- python/tests/test_s_q.py
+++ python/tests/test_s_q.py
@@ -1,43 +1,104 @@
-from code_challenges.Stacks_and_queues.stack_queue import Node, StacksQueues
+import pytest
+from code_challenges.Stacks_and_queues.stack_queue import Node, Stack
 
-def test_successfully_push_onto_a_stack():
-    pass
+def test_successfully_push_onto_a_stack(create_stack):
+    s = create_stack
+    s.push(1)
+    actual = s.head.value
+    expected = 1
+    assert actual == expected
+    # assert stacking.next == None
 
-def test_succ_push_mult_onto_a_stack():
-    pass
+def test_succ_push_mult_onto_a_stack(create_stack):
+    s = create_stack
+    s.push(1)
+    s.push(2)
+    actual = s.head.value
+    expected = 2
+    assert actual == expected
 
-def test_succ_pop_a_stack():
-    pass
+def test_succ_pop_a_stack(create_stack):
+    s = create_stack
+    s.pop()
+    actual = s.head
+    expected = None
+    assert actual == expected
 
-def test_succ_empty_stack_after_mult_props():
-    pass
+def test_succ_empty_stack_after_mult_pops(create_stack, pushing, popping):
+    s = create_stack
+    p = pushing
+    p.pop()
+    p.pop()
+    actual = s.head
+    expected = 1
+    assert actual == expected
 
-def test_succ_peek_next_item_stack():
-    pass
+# def test_succ_peek_next_item_stack():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_instantiate_empty_stack():
-    pass
+# def test_succ_instantiate_empty_stack():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_calling_pop_or_peek_empty_stack_raises_exception():
-    pass
+# def test_calling_pop_or_peek_empty_stack_raises_exception():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_enqueue_into_queue():
-    pass
+# def test_succ_enqueue_into_queue():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_enqueue_multiple_values_into_queue():
-    pass
+# def test_succ_enqueue_multiple_values_into_queue():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_dequeue_out_of_queue_expected_value():
-    pass
+# def test_succ_dequeue_out_of_queue_expected_value():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_peek_into_a_queue_seeing_the_expected_value():
-    pass
+# def test_succ_peek_into_a_queue_seeing_the_expected_value():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_empty_queue_after_multiple_dequeues():
-    pass
+# def test_succ_empty_queue_after_multiple_dequeues():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_succ_instantiate_an_empty_queue():
-    pass
+# def test_succ_instantiate_an_empty_queue():
+#     actual = 
+#     expected = 
+#     assert actual ==
 
-def test_calling_dequeue_or_peek_empty_queue_raises_exception():
-    pass
+# def test_calling_dequeue_or_peek_empty_queue_raises_exception():
+#     actual = 
+#     expected = 
+#     assert actual ==
+
+@pytest.fixture
+def create_stack():
+    s = Stack()
+    return s
+
+@pytest.fixture
+def pushing():
+    p = Stack()
+    p.push(1)
+    p.push(2)
+    p.push(3)
+    return p
+
+@pytest.fixture
+def popping():
+    po = pushing()
+    po.pop()
+    po.pop()
+    return po

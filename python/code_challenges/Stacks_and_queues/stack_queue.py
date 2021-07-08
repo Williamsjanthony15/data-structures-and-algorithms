e8e3713aebc2035b97bc80b2be7f938ccd35b474
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-07-08 00:43:59 -0500

    adding spacing

--- python/code_challenges/Stacks_and_queues/stack_queue.py
+++ python/code_challenges/Stacks_and_queues/stack_queue.py
@@ -1,28 +1,61 @@
 class Node():
     def __init__(self, value=None, next=None):
-        self.value = value,
-        self.next = next,
+        self.value = value
+        self.next = next
 
-class StackQueues():
-    
-    def __init__(self, node=None):
-        self.top = node
+class Stack():
+    """
+    Default
+    """ 
+    def __init__(self):
+        self.top = None
+    """
+    Empty check
+    """
+    def isEmpty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+    """
+    Adding Nodes to a stack in the beginning
+    """
 
-    def append(self):
-
-        stack = []
-        stack.append('a')
-        stack.append('b')
-        stack.append('c')
-        stack.append('d')
-        print('First Stack Attempt')
-        print(stack)
-
-        popOne = stack.pop()
-        popTwo = stack.pop()
-        popThree = stack.pop()
-        popFour = stack.pop()
-
-        popStack = [popOne, popTwo, popThree, popFour]
-        
-        print(popStack)
+    def push(self,value):
+        if self.top == None:
+            self.top = Node(value)
+        else:
+            newNode = Node(value)
+            newNode.next = self.top
+            self.top = newNode
+    """ 
+    Popping if !empty
+    """
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            popNode = self.top
+            self.top = self.top.next
+            popNode.next = None
+            return popNode.value
+    """
+    Looking to see what data is there if any at all
+    """
+    def peek(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.top.data
+    """
+    Displaying the data 
+    """
+    def display(self):
+        displayingNode = self.top
+        if self.isempty():
+            print("empty")
+        else:
+            while(displayingNode != None):
+                print('Beginning', displayingNode.data, end = " ")
+                displayingNode = displayingNode.next
+            return

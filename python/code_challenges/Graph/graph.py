ef48d090e9e92e293abe5e4ee9c7652e4dc6edcb
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-10-04 22:15:49 -0500

    done with graph base implementation

--- python/code_challenges/Graph/graph.py
+++ python/code_challenges/Graph/graph.py
@@ -1,25 +1,10 @@
+
 class Graph:
-    def __init__(self):
-        self._adjacency_list = {}
-
     def add_node(self, value):
-
-        v = Vertex(value)
         self._adjacency_list[v] = []
         return v
 
     def add_edge(self, start_vertex, end_vertex, weight=1):
-
-        if start_vertex not in self._adjacency_list:
-            raise KeyError("Start isnt looking to well")
-        if end_vertex not in self._adjacency_list:
-            raise KeyError("End Not in sight")
-
-        edge = Edge(end_vertex, weight)
-        adjacencies = self._adjacency_list[start_vertex]
-        adjacencies.append(edge)
-
-    def get_nodes(self):
 
         if self._adjacency_list == {}:
             return None
@@ -32,7 +17,7 @@ class Graph:
 
             return nodes
 
-    def get_neighbor(self, node):
+    def get_neighbors(self, node):
 
         nodes = [*self._adjacency_list]
         print(nodes)
@@ -51,29 +36,16 @@ class Graph:
             else:
                 raise KeyError("Null")
 
+    def size(self):
+
+        return len(self._adjacency_list)
+
+
+class Vertex:
+    def __init__(self, value):
+        self.value = value
 class Edge:
-    def __init__(self, vertex, weight=1):
+    def __init__(self, vertex, weight=0):
         self.vertex = vertex
         self.weight = weight
-
-
-if __name__ == "__main__":
-    graph = Graph()
-    a = graph.add_node("a")
-    b = graph.add_node("b")
-    c = graph.add_node("c")
-    d = graph.add_node("d")
-    e = graph.add_node("e")
-    f = graph.add_node("f")
-    g = graph.add_node("g")
-    h = graph.add_node("h")
-    graph.add_edge(a, b)
-    graph.add_edge(a, c)
-    graph.add_edge(b, d)
-    graph.add_edge(c, d)
-    graph.add_edge(a, e)
-    graph.add_edge(e, f)
-    graph.add_edge(e, g)
-    graph.add_edge(f, h)
-    print(graph.get_neighbors(a))
  
